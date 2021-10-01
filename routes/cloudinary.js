8e4a8b919ceb1ed4399732418de07aa3e422ed34
Guille Celma <guillecelmaan@hotmail.es>
2021-10-01 20:23:50 +0200

    GC- feature: added cloudinary routes for product images

--- routes/cloudinary.js
+++ routes/cloudinary.js
@@ -4,15 +4,17 @@ const {cloudinary} = require("../config/cloudinary.config.js")
 router.post( "/upload" , async (req,res)=>{
 
 
+    
+
     try{
 
         const fileStr = req.body.data
-        console.log(fileStr)
+       
         const uploadedResponse = await  cloudinary.uploader.upload(fileStr, {
             upload_preset: "dev_setups"
         })
-        console.log(uploadedResponse)
-        res.json({msg:"funciona"})
+        res.send({response: uploadedResponse.public_id})
+
     }catch(err){
         console.log(err)
         res.status(500).json({err:"upps no va tio"})
